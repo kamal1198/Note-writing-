61fe9a9ea012ea0d11a4e412d389479d0bcea746
kamal1198 <kaur.preetkamal1198@gmail.com>
2022-06-06 15:51:27 -0400

    installed express and added port

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const fs = require ('fs');
 const path = require('path');
+const app = express()
+const port = 3000;
+//To send html file
+// res.sendFile(“public/index.html”)
+
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  app.get('/greeting', (req, res) => {
+    res.send('Hello!')
+  })
+  app.get('/test', (req, res) => {
+    res.send('This is a test')
+  })
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+  })
