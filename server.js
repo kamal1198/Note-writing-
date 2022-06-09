73ee263ed8950cfd3206bb66896cca79573657e8
kamal1198 <kaur.preetkamal1198@gmail.com>
2022-06-09 11:55:56 -0400

    commititng port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require ('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3001;
 
 const util = require ('util')
 const readfiles = util.promisify(fs.readFile)
